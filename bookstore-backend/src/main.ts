0be1a3279b4474734580c973a420f104718020a7
senseei <gabrieldjesus475@gmail.com>
2025-08-27 22:45:48 -0300

    feat(backend): enable CORS and implement global validation pipe with custom error handling
    - Configured CORS to allow all origins and standard HTTP methods.
    - Added a global `ValidationPipe` with custom exception formatting using `ValidationErrorFormatter`.

--- bookstore-backend/src/main.ts
+++ bookstore-backend/src/main.ts
@@ -1,8 +1,37 @@
 import { NestFactory } from '@nestjs/core';
+import { ValidationError } from 'class-validator';
+import { Logger, ValidationPipe } from '@nestjs/common';
+
 import { AppModule } from './app.module';
+
+import { ValidationErrorFormatter } from '@common/validation-error.formatter';
+import { InvalidBodyException } from '@common/exceptions/invalid-body.exception';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.enableCors({
+    origin: '*',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+  });
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      exceptionFactory: (errors: ValidationError[]) => {
+        console.log(errors);
+        const errorMessages = ValidationErrorFormatter.format(errors);
+        return new InvalidBodyException(errorMessages);
+      },
+      transform: true,
+      whitelist: true,
+    }),
+  );
+
   await app.listen(process.env.PORT ?? 3000);
+  Logger.log(
+    `Server running on http://localhost:${process.env.PORT ?? 3000}`,
+    'Bootstrap',
+  );
 }
 bootstrap();
