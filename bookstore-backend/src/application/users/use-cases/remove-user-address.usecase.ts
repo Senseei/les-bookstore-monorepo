f9daed211d550abdca75d1268e4a8e579831215d
senseei <gabrieldjesus475@gmail.com>
2025-09-23 01:27:27 -0300

    refactor(backend): restructure user-related entities and services
    - Moved User and Address entities to a dedicated user directory.
    - Updated import paths in repositories and services to reflect new structure.
    - Removed UsersService and UserWithAddressValidator classes as they were no longer needed.
    - Introduced new CustomerDetails entity to manage user addresses.
    - Added Address and AddressPurpose enums for better address management.
    - Updated validation strategies for user creation and updates.
    - Refactored user DTOs to accommodate new address structure.
    - Enhanced error handling in domain entities.
    - Cleaned up unused imports and organized module dependencies.

--- bookstore-backend/src/application/users/use-cases/remove-user-address.usecase.ts
+++ bookstore-backend/src/application/users/use-cases/remove-user-address.usecase.ts
@@ -6,6 +6,7 @@ import { Injectable } from '@nestjs/common';
 import { CannotRemoveAddressException } from '../exceptions/cannot-remove-address.exception';
 import { UsersService } from '../services';
 
+// TODO REFATORAR
 @Injectable()
 export class RemoveUserAddress {
   constructor(private readonly usersService: UsersService) {}
