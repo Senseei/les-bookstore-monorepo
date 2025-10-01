37e26737a00e27c779ea35e19ddd2eb30a84a155
senseei <gabrieldjesus475@gmail.com>
2025-10-01 20:02:19 -0300

    feat(backend): add refresh token functionality with DTO and service integration

--- bookstore-backend/src/presentation/auth/auth.controller.ts
+++ bookstore-backend/src/presentation/auth/auth.controller.ts
@@ -4,6 +4,7 @@ import { UserDTO } from '@presentation/common/users/dtos';
 import { AuthWebService } from './auth.webservice';
 import { LoginCredentialsDTO } from './dtos/login-credentials.dto';
 import { NewUserDTO } from './dtos/new-user.dto';
+import { RefreshTokenDTO } from './dtos/refresh-token.dto';
 import { JwtToken } from './interfaces';
 
 @Controller('auth')
@@ -20,5 +21,10 @@ export class AuthController {
   @HttpCode(201)
   public async signUp(@Body() body: NewUserDTO): Promise<UserDTO> {
     return await this.authService.signUp(body);
+  }
+
+  @Post('refresh-token')
+  public refreshToken(@Body() body: RefreshTokenDTO): JwtToken {
+    return this.authService.refreshToken(body);
   }
 }
